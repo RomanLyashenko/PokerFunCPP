9956178cf3f4c689d0fc276f14e4f43a9b830434
RomanLyashenko <159914055+RomanLyashenko@users.noreply.github.com>
2024-06-14 18:38:25 +0300

    Update Poker.cpp

--- Poker.cpp
+++ Poker.cpp
@@ -8,10 +8,10 @@ using namespace std;
 int** generateCardsSet() {
     //  suit - ♥3 : ♦4 : ♣5 : ♠6
     int** set = new int* [52];
-    for (int index = 0, value = 2; value < 15; value++) {
+    for (int index=0,value = 2; value < 15; value++) {
         for (int suit = 3; suit < 7; suit++) {
-            set[index++] = new int[2]{ value, suit };
-        }
+            set[index++] = new int[2]{value, suit};
+        }    
     }
     return set;
 }
@@ -44,66 +44,62 @@ void shuffle(int** array) {
     }
 }
 string* createPlayers(int count) {
-    if (count > 6 || count < 3) {
-        cout << "не верное количество игроков" << endl;
-        return nullptr;
-    }
-    else {
-        string* array = new string[count];
-        array[0] = "Игрок";
-        for (int i = 1, flag; i < count; i++) {
-            array[i] = NAMES[random(0, COUNT_NAMES - 1)];
-            flag = false;
-            for (int j = 1; j < i; j++) {
-                if (array[i] == array[j]) {
-                    flag = true;
-                    break;
-                }
-            }
-            if (flag) i--;
+  if (count > 6 || count < 1) {
+    cout << "не верное количество игроков" << endl;
+    return nullptr;
+  }
+  else {
+    string* array = new string[count];
+    array[0] = "Игрок";
+    for (int i = 1, flag; i < count; i++) {
+      array[i] = NAMES[random(0, COUNT_NAMES - 1)];
+      flag = false;
+      for (int j = 1; j < i; j++) {
+        if (array[i] == array[j]) {
+          flag = true;
+          break;
         }
-        return array;
+      }
+      if (flag) i--;
     }
+    return array;
+  }
 
 }
 int* createCash(int countPlayers, int countMoney) {
-    int* cash = new int[countPlayers];
-    for (int i = 0; i < countPlayers; i++) {
-        cash[i] = countMoney;
-    }
-    return cash;
+  int* cash = new int[countPlayers];
+  for (int i = 0; i < countPlayers; i++) {
+    cash[i] = countMoney;
+  }
+  return cash;
 }
 void showPlayers(string*& players, int*& cash, int count) {
-    for (int i = 0; i < count; i++) {
-        cout << players[i] << "\t" << cash[i] << "$" << endl;
-    }
+  for (int i = 0; i < count; i++) {
+    cout << players[i] <<"\t" << cash[i]<<"$" << endl;
+  }
 }
-
 void showPlayer(string players, int cash, int**& playersSet) {
-    cout << players << "\t" << cash << "$" << " [ ";
-    showCards(playersSet);
-    cout << "]" << endl;
+  cout << players << "\t" << cash << "$" << " [ ";
+  showCards(playersSet);
+  cout << "]" <<endl;
 }
-
 void transferTopCard(int**& outSet, int**& inSet) {
-    int countOutSet = _msize(outSet) / sizeof(outSet[0]);
-    int countInSet = _msize(inSet) / sizeof(inSet[0]);
-    int** outSetBuf = new int* [countOutSet - 1];
-    int** inSetBuf = new int* [countInSet + 1];
-    for (int i = 0; i < countOutSet - 1; i++) {
-        outSetBuf[i] = outSet[i];
-    }
-    for (int i = 0; i < countInSet; i++) {
-        inSetBuf[i] = inSet[i];
-    }
-    inSetBuf[countInSet] = outSet[countOutSet - 1];
-    delete[] inSet;
-    delete[] outSet;
-    inSet = inSetBuf;
-    outSet = outSetBuf;
+  int countOutSet = _msize(outSet) / sizeof(outSet[0]);
+  int countInSet = _msize(inSet) / sizeof(inSet[0]);
+  int** outSetBuf = new int* [countOutSet - 1];
+  int** inSetBuf = new int* [countInSet + 1];
+  for (int i = 0; i < countOutSet - 1; i++) {
+    outSetBuf[i] = outSet[i];
+  }
+  for (int i = 0; i < countInSet; i++) {
+    inSetBuf[i] = inSet[i];
+  }
+  inSetBuf[countInSet] = outSet[countOutSet - 1];
+  delete[] inSet;
+  delete[] outSet;
+  inSet = inSetBuf;
+  outSet = outSetBuf;
 }
-
-
 
 int main()
 {
@@ -115,74 +111,36 @@ int main()
     cout << endl;
     int playersCount = 6;
     string* playersName = createPlayers(playersCount);
-    int* cash = createCash(playersCount, DEFAULT_CASH);
-
+    int* cash = createCash(playersCount, 1000);
+    
     int*** playersSets = new int** [playersCount];
     for (int i = 0; i < playersCount; i++) {
-        playersSets[i] = new int* [0];
+      playersSets[i] = new int* [0];
     }
 
     for (int i = 0; i < playersCount; i++) {
-        for (int j = 0; j < 2; j++) {
-            transferTopCard(mainSet, playersSets[i]);
-        }
+      for (int j = 0; j < 2; j++) {
+        transferTopCard(mainSet, playersSets[i]);
+      }     
     }
 
     for (int i = 0; i < playersCount; i++) {
-        showPlayer(playersName[i], cash[i], playersSets[i]);
+      showPlayer(playersName[i], cash[i], playersSets[i]);
     }
 
     int** tableSet = new int* [0];
     for (int i = 0; i < 3; i++) {
-        transferTopCard(mainSet, tableSet);
+      transferTopCard(mainSet, tableSet);
     }
-    
-    cout << "table: ";
+
+    cout << "стол: ";
     showCards(tableSet);
 
-    int blinde = DEFAULT_CASH * 0.2;
-    int smallBlinde = blinde / 2;
-    int indexSmallBlinde = 0;
-    int currentPlayer;
-
     while (true) {
-        
-        if (indexSmallBlinde > playersCount - 1) indexSmallBlinde = 0;
-        currentPlayer = indexSmallBlinde;
-        
-        if (cash[indexSmallBlinde] >= smallBlinde) {
-            cash[currentPlayer++] -= smallBlinde;
-        }
-        if (cash[indexSmallBlinde] >= blinde) {
-            cash[currentPlayer++] -= blinde;
-        }
-        for (int i = 0; i < indexSmallBlinde, i < playersCount; i++) {
-            cout << "enter your blinde: ";
-            int currentBlinde;
-            cin >> currentBlinde;
-            if (currentBlinde == 0) {
-                cout << "player checked" << endl;
-                continue;
-            }
-            else if (currentBlinde < blinde) {
-                cout << "insufficient blinde";
-                i--;
-                continue;
-            }
-            else if (currentBlinde < cash[i]) {
-                cout << "not enough money";
-                i--;
-                continue;
-            }
-            else{
-                cout << "blinde accepted" << endl;
-                cash[i] -= currentBlinde;
-            }
-        }
-        for (int i = 0; i < indexSmallBlinde; i++) {
 
-        }
     }
-
+    
+    
+}
 
 }
